98a6047bebb78f333f112a71d559cfc9bf1fb3e0
Awais <awaimusl@gmail.com>
2017-12-04 23:54:17 +0500

    fix the layout of topbar

--- resources/views/pages/integrator.blade.php
+++ resources/views/pages/integrator.blade.php
@@ -18,39 +18,7 @@
 
     <body class="with-navbar">
         <!-- Navigation -->
-        <nav class="navbar navbar-inverse navbar-fixed-top">
-            <div class="container">
-                <div class="navbar-header">
-                    <!-- Collapsed Hamburger -->
-                    <div class="hamburger">
-                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#spark-navbar-collapse">
-                            <span class="sr-only">Toggle Navigation</span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                        </button>
-                    </div>
-
-                    <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/home') }}">
-                        <img src="{{ asset('img/mono-logo.png') }}" style="height: 32px;">
-                    </a>
-                </div>
-
-                <div class="collapse navbar-collapse" id="spark-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                        &nbsp;
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="nav navbar-nav navbar-right">
-                        <li><a href="{{ route('login') }}" class="navbar-link">Login</a></li>
-                        <li><a href="{{ route('register') }}" class="navbar-link">Register</a></li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+        @include("layouts.partials.nav-bar-top")
 
         <div class="container">
 
@@ -107,11 +75,7 @@
         </div><!--/.container-->
 
         <hr>
-        <footer class="footer">
-            <div class="container">
-                <p class="text-muted">Copyright © Footer {{ config('app.name') }} Plugin 2017. All right reserved.</p>
-            </div>
-        </footer>
+        @include("layouts.partials.nav-footer")
 
         <script src="{{ asset('js/app.js') }}"></script>
     </body>

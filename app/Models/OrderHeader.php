07262d20d5013bf1a7a643a1b3a354817617b73c
Awais <awaismusl@live.com>
2017-12-24 21:57:51 +0500

    delivery_date on save date fixed

--- app/Models/OrderHeader.php
+++ app/Models/OrderHeader.php
@@ -2,9 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
 class OrderHeader extends Model
 {
     protected $guarded = [];
+
+    protected static function boot()
+    {
+        static::addGlobalScope('user_id', function (Builder $builder) {
+            $builder->where('user_id', auth()->user()->id);
+        });
+    }
+
+    public function setDeliveryDateAttribute($value) {
+        $this->attributes['delivery_date'] = $value == '0000-00-00'?null:$value;
+    }
 }

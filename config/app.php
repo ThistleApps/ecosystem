b3199a5d6987b5f15e99b97bafc6906920109a69
Awais <awaismusl@live.com>
2017-12-31 19:40:42 +0500

    symblink fixd

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Blend App'),
+    'name' => env('APP_NAME', 'Blend Retail'),
 
     /*
     |--------------------------------------------------------------------------
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'http://blendretail.com'),
 
     /*
     |--------------------------------------------------------------------------

2b1c90aeee60e0a91b30a4a0cb4f78d045752043
Muhammad Awais <awaismusl@live.com>
2018-02-09 23:00:16 +0500

    test seed class runing

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -7,12 +7,17 @@ use App\Models\AdminSetting;
 use App\Models\Remote\ROrderHeader;
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Artisan;
 use Laravel\Spark\Spark;
+use Symfony\Component\Process\Process;
 
 class TestController extends Controller
 {
     public function index() {
-        dd(auth()->user()->posType);
+        $process = new Process('php artisan db:seed --class=PosTypeSeeder');
+        $process->setWorkingDirectory('../');
+        $process->run();
+        dd($process->getOutput());
 //        (new \AdminSettingsTableSeeder())->run();
 //        $this->dispatch(new MerchantDataTransectionJob(auth()->user()));
     }

668714b5cf314cdbafe9cc99bb558fbc9acff4d5
Muhammad Awais <awaismusl@live.com>
2018-03-21 10:51:15 +0500

    stripe working now

--- config/services.php
+++ config/services.php
@@ -31,8 +31,8 @@ return [
 
     'stripe' => [
         'model'  => App\User::class,
-        'key'    => env('STRIPE_KEY'),
-        'secret' => env('STRIPE_SECRET'),
+        'key'    => env('STRIPE_KEY' , config('admin_settings.stripe_pub')),
+        'secret' => env('STRIPE_SECRET' , config('admin_settings.stripe_sec')),
     ],
 
 ];

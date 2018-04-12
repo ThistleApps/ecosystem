74d948f493e958ea6057253d0bda6dfc528e7a57
thistleapps <mduncan77@comcast.net>
2018-04-11 21:15:00 -0400

    Ticketit Added Resolved GitIgnore

---
+++ vendor/kordy/ticketit/src/Helpers/Cdn.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Kordy\Ticketit\Helpers;
+
+/**
+ * This file containes the version of different external JS and CSS libraries
+ * that are included.
+ *
+ * Class Cdn
+ */
+class Cdn
+{
+    const CodeMirror = '5.34.0';
+    const Summernote = '0.8.9';
+    const FontAwesome = '4.7.0';
+
+    // https://datatables.net/download/
+    const DataTables = '1.10.16';
+    const DataTablesResponsive = '2.2.1';
+}

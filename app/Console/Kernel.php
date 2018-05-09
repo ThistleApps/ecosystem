dddbc548b79fab275e2783b614c146d20b342e37
Muhammad Awais <awaismusl@live.com>
2018-05-09 23:03:54 +0500

    getswift schedule command schedule time changing for testing perpose

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -29,7 +29,7 @@ class Kernel extends ConsoleKernel
                   ->hourly()->withoutOverlapping();
 
         $schedule->command('sync:getswift-deliveries')
-            ->hourly()->withoutOverlapping();
+            ->everyFiveMinutes()->withoutOverlapping();
     }
 
     /**

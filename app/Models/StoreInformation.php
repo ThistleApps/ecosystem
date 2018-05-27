76656cfceb8ef5a80e9b3709c246a28b70cbd2cf
Rizwan Aslam <rizwan@codebrisk.com>
2018-05-27 13:55:17 +0500

    store information of delivery fixed

--- app/Models/StoreInformation.php
+++ app/Models/StoreInformation.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class StoreInformation extends Model
 {
@@ -17,6 +18,14 @@ class StoreInformation extends Model
     public function getFullAddressAttribute()
     {
         return $this->addr_line1.', '.$this->addr_line2.(!empty($this->addr_line2)?', ':'').$this->addr_line3;
+    }
+
+    protected static function boot()
+    {
+
+        static::addGlobalScope('user_id', function (Builder $builder) {
+            $builder->where('user_id', auth()->user()->id);
+        });
     }
 
 

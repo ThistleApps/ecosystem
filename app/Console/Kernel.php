40a6a435057e59ce81c470d1f1ca873fb6ee74b7
thistleapps <mduncan77@comcast.net>
2018-07-03 17:33:19 -0400

    Updated Mailchimp Integration Schedule - Fixed interval timing

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -37,7 +37,7 @@ class Kernel extends ConsoleKernel
             ->hourly()->withoutOverlapping();
 
         $schedule->command('mailchimp:transactions')
-            ->nightly()->withoutOverlapping();
+            ->daily()->withoutOverlapping();
     }
 
     /**

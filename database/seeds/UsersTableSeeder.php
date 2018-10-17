83001bd392f4d1569ed5e95272de174cb639465c
thistleapps <mduncan77@comcast.net>
2018-10-17 11:10:44 -0400

    UserTableSeeder added

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //See users table with one base user for first install
+        DB::table('users')->insert([
+            'name' => str_random(10),
+            'email' => str_random(10).'@gmail.com',
+            'password' => bcrypt('secret'),
+
+        ]);
+    }
+}

1aa805ba0ffc1a86d703417576871bd6435f07f4
ThistleApps <mduncan77@comcast.net>
2019-07-20 09:24:02 -0400

    Troubleshooting getSwift integration

--- app/Console/Commands/getswiftDeliveriesUpload.php
+++ app/Console/Commands/getswiftDeliveriesUpload.php
@@ -66,7 +66,7 @@ class getswiftDeliveriesUpload extends Command
                 if (\Carbon\Carbon::parse($order_header->creation_date)->lessThan(Carbon::now()->addMonth()))
                     continue;
 
-                $url = config('getswift.base_url').config('getswift.deliveries');
+                $url = config('getswift.base_url');
 
                 $request_data = $this->mappingGetswiftFeilds($order_header , $getswift_key);
 

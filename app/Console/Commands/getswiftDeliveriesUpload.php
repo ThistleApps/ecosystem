56a121841cfa1dcf3b24f2cf6994bc0cb01e034b
ThistleApps <mduncan77@comcast.net>
2019-07-21 09:22:03 -0400

    TS logger

--- app/Console/Commands/getswiftDeliveriesUpload.php
+++ app/Console/Commands/getswiftDeliveriesUpload.php
@@ -58,13 +58,18 @@ class getswiftDeliveriesUpload extends Command
                 Log::info("getswift order sync: this merchant ".auth()->user()->email. " not have the getswift key");
                 continue;
             }
+            Log::info("Log before order header 1");
 
             $order_headers = OrderHeader::query()->where(function ($q){
                 $q->whereNull('getswift_status')->orWhere('getswift_status' , OrderHeader::DELIVERY_NEW);
             })->orderByDesc('id')->get();
 
+            Log::info("Log after order header 2");
+
             foreach ($order_headers as $order_header)
             {
+                Log::info("inside loop 1");
+
                 if (\Carbon\Carbon::parse($order_header->creation_date)->lessThan(Carbon::now()->addMonth()))
                     continue;
 
